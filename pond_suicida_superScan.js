5e376250ed7cfd0c500267aab61a0c3c6aea4eb3
Jonatas Leon <jonatasleon@users.noreply.github.com>
2016-08-31 14:27:43 -0300

    Create pond_suicida_superScan.js

---
+++ pond_suicida_superScan.js
@@ -0,0 +1,60 @@
+var range;
+var angle = 0;
+var nAngle;
+
+while(true) {
+  var res = superScan(angle);
+  
+  if(res.found)
+    angle = res.angle;
+  else
+    angle += 5;
+    
+  swim(angle);
+}
+function superScan(angle) {
+  var aux = 0;
+  var range;
+  var angleRight = angle;
+  
+  var res = {};
+  res.found = false;
+  
+  range = scan(angle + aux);
+  if(range <= 70) {
+    cannon(angle + aux , range);
+    res.found = true;
+    res.angle = angle + aux;
+  }
+  aux += 90;
+  
+  range = scan(angle + aux);
+  if(range <= 70) {
+    cannon(angle + aux , range);
+    res.found = true;
+    res.angle = angle + aux;
+  }
+  aux += 90;
+  
+  range = scan(angle + aux);
+  if(range <= 70) {
+    cannon(angle + aux , range);
+    res.found = true;
+    res.angle = angle + aux;
+  }
+  aux += 90;
+  
+  range = scan(angle + aux);
+  if(range <= 70) {
+    cannon(angle + aux , range);
+    res.found = true;
+    res.angle = angle + aux;
+  }
+  aux += 90;
+  
+  return res;
+}
+
+function min(n1, n2) {
+  return (n1 < n2) ? n1 : n2;
+}
