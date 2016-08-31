8af677fe7e9bfd30c4b0bb038b98a140d33ccd7b
Jonatas Leon <jonatasleon@users.noreply.github.com>
2016-08-31 09:15:48 -0300

    Patinho fudidex agr

--- super_pond.js
+++ super_pond.js
@@ -1,13 +1,16 @@
 var right = true;
 var angle = 0;
 var range;
+var aux;
 
 while(loc_x() < 50) {
   swim(0);
+  superScan(angle+=5);
 }
 
 while(loc_y() > 50) {
   swim(270);
+  superScan(angle+=5);
 }
 
 
@@ -15,24 +18,39 @@ while(true) {
   
   if(loc_x() <= 90 && right) {
     swim(0);
-  } else if(!(right = !(loc_x() >= 10))) {
+  } else if(!(right = (loc_x() < 10))) {
     swim(180);
     right = false;
   } else {
     right = true;
   }
   
-  superScan(angle+=15);
+  aux = 0;
+  
+  superScan(angle+=5);
 }
 
 function superScan(angle) {
   var aux = 0;
   var range;
-  while(aux <= 360) {
-    range = scan(angle + aux);
-    if(range <= 70)
-      cannon(angle + aux , range);
-    aux += 90;
-  }
+  
+  range = scan(angle + aux);
+  if(range <= 70)
+    cannon(angle + aux , range);
+  aux += 90;
+  
+  range = scan(angle + aux);
+  if(range <= 70)
+    cannon(angle + aux , range);
+  aux += 90;
+  
+  range = scan(angle + aux);
+  if(range <= 70)
+    cannon(angle + aux , range);
+  aux += 90;
+  
+  range = scan(angle + aux);
+  if(range <= 70)
+    cannon(angle + aux , range);
+  aux += 90;
 }
-
